23d88bd34841007d1f4cdef098f8c82970554791
joshuahugs2000-creator <joshuahugs2000@gmail.com>
2026-06-08 10:13:07 +0000

    Change request_type to use enum names instead of values

--- database/models.py
+++ database/models.py
@@ -83,7 +83,7 @@ class PendingRequest(Base):
     id           = Column(Integer, primary_key=True, autoincrement=True)
     from_user_id = Column(BigInteger, ForeignKey("users.user_id"), nullable=False)
     to_user_id   = Column(BigInteger, ForeignKey("users.user_id"), nullable=False)
-    request_type = Column(SAEnum(RequestType, values_callable=lambda x: [e.value for e in x]), nullable=False)
+    request_type = Column(SAEnum(RequestType, values_callable=lambda x: [e.name for e in x]), nullable=False)
     group_id     = Column(BigInteger, nullable=False)
     message_id   = Column(BigInteger, nullable=True)
     expires_at   = Column(DateTime, nullable=False)
